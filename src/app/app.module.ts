69a33c6facaf99bf58c12e823be1880b82b3cd48
nuri1977 <nurilacka@gmail.com>
2020-05-27 23:58:30 +0200

    Data binding example in Home Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  clickCounter:number = 0;
+  name: string = '';
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  countClick() {
+    this.clickCounter += 1;
   }
 
 }
