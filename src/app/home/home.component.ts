97938685a13c25546ab9761e46a79e8eff4a0e5f
nuri1977 <nurilacka@gmail.com>
2020-05-28 00:32:42 +0200

    Class binding

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,4 +19,12 @@ export class HomeComponent implements OnInit {
     this.clickCounter += 1;
   }
 
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4,
+    }
+    return myClasses;
+  }
+
 }
